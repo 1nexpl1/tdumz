bfe032638866496d3101a14ad118eabcd2c2a8c8
Артур Сабиров <artursabirov@MacBook-Air-Artur.local>
2025-04-23 16:22:18 +0500

    fix import server

--- server/dtos/userDto.js
+++ server/dtos/userDto.js
@@ -1,9 +1,11 @@
-module.exports = class UserDto {
+class UserDto {
     login;
     id;
-
+  
     constructor(model) {
-        this.login = model.login;
-        this.id = model.id;
+      this.login = model.login;
+      this.id = model.id;
     }
-}
+  }
+  
+  export default UserDto;
