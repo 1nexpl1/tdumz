b33feb8cef08f07a8b3e2d10db27a0c5165a3ae7
Артур Сабиров <artursabirov@MacBook-Air-Artur.local>
2025-05-11 03:04:56 +0500

    helmet provider

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,13 @@ import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { fetchTypes } from './http/itemAPI';
+import { HelmetProvider } from 'react-helmet-async';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
 root.render(
-    <App/>
+    <HelmetProvider>
+        <App />
+    </HelmetProvider>
 );
