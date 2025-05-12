afa4b475c0c46a7c2429c1d8af22b60d01dbd48a
Артур Сабиров <artursabirov@MacBook-Air-Artur.local>
2025-05-12 17:16:16 +0500

    small nerf bags

--- admin/src/components/AppRouter/AppRouter.jsx
+++ admin/src/components/AppRouter/AppRouter.jsx
@@ -7,19 +7,13 @@ import Client from '../client/client';
 const AppRouter = ({ isAuth }) => {
   return (
     <Routes>
-      {isAuth ? (
-        <>
           <Route path="dashboard" element={<DashboardPage />} />
           <Route path="item" element={<Items />} />
           <Route path="client" element={<Client />} />
           <Route path="*" element={<Navigate to="dashboard" />} />
-        </>
-      ) : (
-        <>
           <Route path="signin" element={<SignInPage />} />
           <Route path="*" element={<Navigate to="signin" />} />
-        </>
-      )}
+      
     </Routes>
   );
 };
